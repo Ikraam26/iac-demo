ea77b284b93f2b0a3447f5dd86c8d2f59e7b7baf
Ikraam26 <ikraamamr@gmail.com>
2025-05-07 12:22:54 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,35 +6,26 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_credentials_validation = true
   endpoints {
-    ec2 = "http://ip10-0-15-4-d0dgrso05akh4glkf950-4566.direct.lab-boris.fr/"
+    ec2 = "http://ip10-0-15-4-d0dgrso05akh4glkf950-4566.direct.lab-boris.fr"
   }
 }
 
-# ==== Déclaration de variables pour personnalisation ====
-
-variable "timestamp" {
-  description = "Un identifiant unique pour créer une instance unique"
-  type        = string
+variable "instance_count" {
+  description = "Nombre d'instances à créer"
+  type        = number
+  default     = 3
 }
 
-variable "ami_id" {
-  description = "AMI aléatoire"
-  type        = string
-}
-
-# ==== Création de l'instance EC2 avec un nom UNIQUE ====
-
-resource "aws_instance" "ec2_instance_${var.timestamp}" {
-  ami           = var.ami_id
+resource "aws_instance" "demo" {
+  count         = var.instance_count
+  ami           = "ami-12345678"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "GitHubInstance-${var.timestamp}"
+    Name = "Demo-Instance-${count.index + 1}"
   }
 }
 
-# ==== Output pour récupérer l'ID de l'instance ====
-
-output "instance_id" {
-  value = aws_instance.ec2_instance_${var.timestamp}.id
+output "instance_ids" {
+  value = aws_instance.demo[*].id
 }
